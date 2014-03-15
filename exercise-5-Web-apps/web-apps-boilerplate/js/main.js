3938be28ddcc558bdec634291f3080a03cd94249
vxvinod <vinodkmrmurugan@gmail.com>
2014-03-15 23:36:12 +0530

    web app using backbone app

---
+++ exercise-5-Web-apps/web-apps-boilerplate/js/main.js
@@ -0,0 +1,35 @@
+require.config({
+	paths:{
+		'jquery': 'libs/jquery-1.10.1.min',
+		'bootstrap':'libs/bootstrap',
+		'underscore':'libs/underscore',
+		'backbone':'libs/backbone'
+	},
+
+	shim: {
+		'underscore':{
+			exports:'_'
+		},
+		'backbone':{
+			deps:[
+				'underscore',
+				'jquery'
+			]
+		},
+		'bootstrap':{
+			deps:[
+				'jquery'
+			]
+		}
+	}
+});
+
+
+require([
+	'backbone',
+	'app/app'
+	],function(Backbone,App){
+		'use strict';
+		console.log(Backbone);
+		App.initialize();
+	});
